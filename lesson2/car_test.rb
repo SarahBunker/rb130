b8798ef2e0898cd8ff0cc87f091dc6e256dfa21a
SarahBunker <ChemEnCoder@gmail.com>
2021-11-19 22:57:12 +0000

    adding files to test assert_equal and assert_same

--- lesson2/car_test.rb
+++ lesson2/car_test.rb
@@ -39,3 +39,16 @@ class CarTest < MiniTest::Test
   private
   attr_reader :car
 end
+
+class CarTest2 < MiniTest::Test
+  def test_value_equality
+    car1 = Car.new
+    car2 = Car.new
+
+    car1.name = "Kim"
+    car2.name = "Kim"
+
+    assert_equal(car1, car2)          # this will pass
+    assert_same(car1, car2)           # this will fail
+  end
+end
