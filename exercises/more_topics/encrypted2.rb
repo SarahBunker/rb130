f7e78edb2e4e22766b93dd13981d26124fb3b551
SarahBunker <ChemEnCoder@gmail.com>
2021-11-22 15:59:55 +0000

    3rd draft using String#tr method.

--- exercises/more_topics/encrypted2.rb
+++ exercises/more_topics/encrypted2.rb
@@ -20,18 +20,24 @@ ENCRYPTED_PIONEERS = [
   'Tregehqr Oynapu',
 ].freeze
 
-def decrypt_char(encrypted_char)
-  case encrypted_char
-  when 'a'..'m', 'A'..'M' then (encrypted_char.ord + 13).chr
-  when 'n'..'z', 'N'..'Z' then (encrypted_char.ord - 13).chr
-  else                         encrypted_char
-  end
-end
+# def decrypt_char(encrypted_char)
+#   case encrypted_char
+#   when 'a'..'m', 'A'..'M' then (encrypted_char.ord + 13).chr
+#   when 'n'..'z', 'N'..'Z' then (encrypted_char.ord - 13).chr
+#   else                         encrypted_char
+#   end
+# end
+
+# def rot13(encrypted_text)
+#   encrypted_text.each_char.reduce('') do |result, encrypted_char|
+#     result + decrypt_char(encrypted_char)
+#   end
+# end
 
 def rot13(encrypted_text)
-  encrypted_text.each_char.reduce('') do |result, encrypted_char|
-    result + decrypt_char(encrypted_char)
-  end
+  alphabet = ("a".."z").to_a.join
+  encrypted_text.tr!(alphabet.upcase, alphabet.upcase[13,13] + alphabet.upcase[0,13])
+  encrypted_text.tr!(alphabet.upcase, alphabet.upcase[13,13] + alphabet.upcase[0,13])
 end
 
 ENCRYPTED_PIONEERS.each do |encrypted_text|
