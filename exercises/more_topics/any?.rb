4fed4a959aecc9ebdbba57f05a1e07af5af5e503
SarahBunker <ChemEnCoder@gmail.com>
2021-11-25 15:08:23 +0000

    all? and any? methods practice passing blocks

---
+++ exercises/more_topics/any?.rb
@@ -0,0 +1,15 @@
+
+def any?(collection,&block)
+  collection.each { |item| return true if block.call(item) }
+  false
+end
+
+p any?([1, 3, 5, 6]) { |value| value.even? } == true
+p any?([1, 3, 5, 7]) { |value| value.even? } == false
+p any?([2, 4, 6, 8]) { |value| value.odd? } == false
+p any?([1, 3, 5, 7]) { |value| value % 5 == 0 } == true
+p any?([1, 3, 5, 7]) { |value| true } == true
+p any?([1, 3, 5, 7]) { |value| false } == false
+p any?([]) { |value| true } == false
+puts
+p any?({1 => 3, 2 => 4, 3 => 3}) {|key,value| key == value } == true
