15c337a779b0b3b3adbb15c9111bff3bff84a099
SarahBunker <ChemEnCoder@gmail.com>
2021-11-30 23:39:12 +0000

    adding updates to algorythm

--- exercises/challenges/perfect_numbers_pedac.rb
+++ exercises/challenges/perfect_numbers_pedac.rb
@@ -1,6 +1,13 @@
 =begin
 #-------------------------------------------------
-PROBLEM:
+PROBLEM: determine if a number is perfect, abundant, or deficient
+
+Perfect numbers have an Aliquot sum that is equal to the original number.
+Abundant numbers have an Aliquot sum that is greater than the original number.
+Deficient numbers have an Aliquot sum that is less than the original number.
+  prime numbers are always deficient because thier only divisor is 1
+
+Aliquot sum is the sum of positive divors(# that can be evenly devided, no remainder excluding the number itself)
 
 input: 
 output:
@@ -9,6 +16,7 @@ rules:
   Explicit requirements:
     
   Implicit requirements:
+    raise error if given negative input
     
 Unclear parts of problem?
 Edge cases?
@@ -23,9 +31,19 @@ DATA STRUCTURE
 ALGORITHM
 example:
 
-
 class methods:
-
+::classify(number)
+  return "deficient" if number is prime
+  pass number to aliquot sum method
+  helper aliquot sum
+    calculate all divisors
+    store in array
+    sum array
+  compare original number - result
+  if positive return "deficient"
+  elif 0 return "perfect"
+  else return "abundant"
+output string
 instance methods:
 
 =end
